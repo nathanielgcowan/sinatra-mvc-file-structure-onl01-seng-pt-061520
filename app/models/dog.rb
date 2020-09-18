e44aafa444010e382232412eff92b68aecc676c7
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-18 23:53:41 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+  attr_accessor :name, :breed, :age
+  @dogs = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
+  end
+
+  def self.all
+    @dogs
+  end
 
 end
